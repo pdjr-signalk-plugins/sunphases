3247c6214a063e40bda7fec075bcfe9151da62f2
Paul Reeve <preeve@pdjr.eu>
2024-09-17 09:42:04 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,10 @@ const MyApp = require('signalk-libapp/App.js');
 const Log = require('signalk-liblog/Log.js');
 const Delta = require('signalk-libdelta/Delta.js');
 
-const PLUGIN_ID = 'sunphases';
-const PLUGIN_NAME = 'pdjr-signalk-sunphases';
-const PLUGIN_DESCRIPTION = 'Inject sunlight phase paths into Signal K';
-const PLUGIN_SCHEMA = {
+const PLUGIN_ID: string = 'sunphases'
+const PLUGIN_NAME: string = 'pdjr-signalk-sunphases'
+const PLUGIN_DESCRIPTION: string = 'Inject sunlight phase paths into Signal K'
+const PLUGIN_SCHEMA: object = {
   "type": "object",
   "properties": {
     "positionkey": {
@@ -165,168 +165,202 @@ const PLUGIN_SCHEMA = {
     }
   },
   "required": [ "notifications" ]
-};
-const PLUGIN_UISCHEMA = {};
+}
+const PLUGIN_UISCHEMA: object = {}
 
-module.exports = function (app) {
-  var plugin = {};
-  var unsubscribes = [];
+module.exports = function (app: any) {
+  let unsubscribes: (() => void)[] = []
 
-  plugin.id = PLUGIN_ID;
-  plugin.name = PLUGIN_NAME;
-  plugin.description = PLUGIN_DESCRIPTION;
-  plugin.schema = PLUGIN_SCHEMA;
-  plugin.uischema = PLUGIN_UISCHEMA;
+  let options: SKOptions = {
+    heartbeat: 0,
+    lastday: 0,
+    lastposition: { latitude: 0, longitude: 0 },
+    metadata: [],
+    notifications: [],
+    positionkey: '',
+    root: '',
+    times: null
+  }
 
-  const App = new MyApp(app);
-  const log = new Log(plugin.id, { ncallback: app.setPluginStatus, ecallback: app.setPluginError });
-  const delta = new Delta(app, plugin.id);
+  const plugin: SKPlugin = {
 
-  plugin.start = function(options) {
+    id: PLUGIN_ID,
+    name: PLUGIN_NAME,
+    description: PLUGIN_DESCRIPTION,
+    schema: PLUGIN_SCHEMA,
+    uiSchema: PLUGIN_UISCHEMA,
+
+    start: function(props: any) {
+      const delta = new Delta(app, plugin.id)
 
     // Make options with global scope as plugin.options.
-    plugin.options = {}
-    plugin.options.positionkey = (options.positionkey || plugin.schema.properties.positionkey.default);
-    plugin.options.root = (options.root || plugin.schema.properties.root.default);
-    plugin.options.heartbeat = (options.heartbeat || plugin.schema.properties.heartbeat.default);
-    plugin.options.notifications = (options.notifications || plugin.schema.properties.notifications.default);
-    plugin.options.metadata = (options.metadata || plugin.schema.properties.metadata.default);
+    options.positionkey = (props.positionkey || plugin.schema.properties.positionkey.default)
+    options.root = (props.root || plugin.schema.properties.root.default)
+    options.heartbeat = (props.heartbeat || plugin.schema.properties.heartbeat.default)
+    options.notifications = (props.notifications || plugin.schema.properties.notifications.default)
+    options.metadata = (props.metadata || plugin.schema.properties.metadata.default)
 
-    log.N(`maintaining keys in '${plugin.options.root}' (heartbeat is ${plugin.options.heartbeat}s)`);
+    app.setPluginStatus(`maintaining keys in '${options.root}' (heartbeat is ${options.heartbeat}s)`)
 
     // Publish meta information for all maintained keys.
-    if (plugin.options.metadata) {
-      plugin.options.metadata.map(entry => delta.addMeta(plugin.options.root + entry.key, { "description": entry.description, "units": entry.units }));
+    if (options.metadata !== undefined) {
+      options.metadata.forEach((entry) => delta.addMeta(options.root + entry.key, { "description": entry.description, "units": entry.units }));
       delta.commit().clear();
     }
       
     // Get a stream that reports vessel position and sample it at the
     // requested interval.
-    plugin.options.lastday = 0;
-    plugin.options.lastposition = { latitude: 0.0, longitude: 0.0 };
+    options.lastday = 0;
+    options.lastposition = { latitude: 0.0, longitude: 0.0 };
 
-    var positionStream = app.streambundle.getSelfStream(plugin.options.positionkey);
+    let positionStream = app.streambundle.getSelfStream(options.positionkey);
     if (positionStream) { 
-      positionStream = (plugin.options.heartbeat == 0)?positionStream.take(1):positionStream.debounceImmediate(plugin.options.heartbeat * 1000);
-      unsubscribes.push(positionStream.onValue(position => {
-        var now = new Date();
-        var today = dayOfYear(now);
+      positionStream = (options.heartbeat == 0)?positionStream.take(1):positionStream.debounceImmediate(options.heartbeat * 1000);
+      unsubscribes.push(positionStream.onValue((position: { latitude: number, longitude: number }) => {
+        let now = new Date();
+        let today = dayOfYear(now);
 
         // Add sunphase key updates to <deltas> if this is the first
         // time around or if we have entered a new day or if our
         // position has changed significantly.
-        if ((plugin.options.lastday != today) || (Math.abs(plugin.options.lastposition.latitude - position.latitude) > 1.0) || (Math.abs(plugin.options.lastposition.longitude - position.longitude) > 1.0)) {
-          if (plugin.options.lastday != today) {
-            app.debug(`updating sunphase data for day change from ${plugin.options.lastday} to ${today}`);
+        if ((options.lastday != today) || (Math.abs(options.lastposition.latitude - position.latitude) > 1.0) || (Math.abs(options.lastposition.longitude - position.longitude) > 1.0)) {
+          if (options.lastday != today) {
+            app.debug(`updating sunphase data for day change from ${options.lastday} to ${today}`);
           } else {
-		        app.debug(`updating sunphase data for position change from ${JSON.stringify(plugin.options.lastposition)} to ${JSON.stringify(position)}`);
+		        app.debug(`updating sunphase data for position change from ${JSON.stringify(options.lastposition)} to ${JSON.stringify(position)}`);
           }
 
-          if (plugin.options.times = suncalc.getTimes(now, position.latitude, position.longitude)) {
-            Object.keys(plugin.options.times).forEach(k => delta.addValue(plugin.options.root + k, plugin.options.times[k]));
+          if (options.times = suncalc.getTimes(now, position.latitude, position.longitude)) {
+            Object.keys(options.times).forEach(k => delta.addValue(options.root + k, options.times[k]));
           } else {
-            log.E("unable to compute sun phase data", false);
+            app.setPluginError("unable to compute sun phase data", false);
           }
           delta.commit().clear();
               
-          plugin.options.lastday = today;
-          plugin.options.lastposition = position;
+          options.lastday = today;
+          options.lastposition = position;
         }
 
         // Check that we actually recovered sun phase data into
         // <options.times> and if so, add notification key updates to
         // <deltas>.
-        if (plugin.options.times) {
-          plugin.options.notifications.forEach(notification => {
+        if (options.times) {
+          options.notifications.forEach((notification: any) => {
             try {
               // Build in-range and out-range notification paths.
-              var nirpath = "notifications." + plugin.options.root + notification.inrangenotification.key;
-              var norpath = "notifications." + plugin.options.root + notification.outrangenotification.key;
+              var nirpath = "notifications." + options.root + notification.inrangenotification.key;
+              var norpath = "notifications." + options.root + notification.outrangenotification.key;
               // Get the times of interest as seconds in this day.
-              now = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
-              var start = parseTimeString(notification.rangelo, plugin.options.times);
-              var end = parseTimeString(notification.rangehi, plugin.options.times);
+              let seconds: number = (now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds();
+              let start = parseTimeString(notification.rangelo, options.times);
+              let end = parseTimeString(notification.rangehi, options.times);
               // Is this an "in-range" or "out-of-range" update?
-              if ((now > start) && (now < end)) {
+              if ((seconds > start) && (seconds < end)) {
                 // It's "in-range". Is there an "in-range" notification path?
                 // If so and we haven't made this update already...
                 if (notification.inrangenotification.key && (!notification.actioned || (notification.actioned != 1))) {
                   // Add a create in-range notification delta to deltas.
-                  App.notify(nirpath, {
+                  app.notify(nirpath, {
                     state: notification.inrangenotification.state || "normal",
                     method: notification.inrangenotification.method || [],
                     message: `Between ${notification.rangelo} and ${notification.rangehi}.`
                   }, plugin.id);
                   // And add a delete out-of-range notification delta to deltas.
-                  App.notify(norpath, null, plugin.id);
+                  app.notify(norpath, null, plugin.id);
                   notification.actioned = 1;
                 }
               } else {
                 if (notification.outrangenotification.key && (!notification.actioned || (notification.action    != -1))) {
-                  App.notify(norpath, {
+                  app.notify(norpath, {
                     state: notification.outrangenotification.state || "normal",
                     method: notification.outrangenotification.method || [],
                     message: `Outside ${notification.rangelo} and ${notification.rangehi}.`
                   }, plugin.id);
-                  App.notify(nirpath, null,plugin.id);
+                  app.notify(nirpath, null,plugin.id);
                   notification.actioned = -1;
                 }
               }                            
             } catch(e) {
-              log.E(e);
+              if (e instanceof Error) app.debug(e.message);
             }
           });
         }
       }));
     } else {
-      log.E(`cannot obtain vessel position from '${plugin.options.positionkey}'`);
+      app.setPluginError(`cannot obtain vessel position from '${options.positionkey}'`)
+    }
+    },
+
+    stop: function () {
+      unsubscribes.forEach(f => f())
     }
   }
 
-  plugin.stop = function () {
-    plugin.unsubscribes.forEach(f => f())
-  }
+  return(plugin)
+}
 
-  function dayOfYear(date){
-    return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
-  }
+function dayOfYear(date: Date): number {
+  return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000
+}
 
-  function parseTimeString(s, sunphases) {
-    var retval, matches, date;
-    s = s.trim();
-    if (matches = s.match(/^(\d\d):(\d\d):(\d\d)$/)) {
-      if (((1 * matches[1]) < 24) && ((1 * matches[2]) < 60) && ((1 * matches[3]) < 60)) {
-        retval = (3600 * matches[1]) + (60 * matches[2]) + (1 * matches[3]);
-      } else {
-        throw "hh:mm:ss value is invalid";
-      }
-    } else if (matches = s.match(/^(\w+)$/)) {
-      if (sunphases.hasOwnProperty(matches[1])) {
-        date = sunphases[matches[1]];
-        retval = (3600 * date.getHours()) + (60 * date.getMinutes()) + (1 * date.getSeconds());
-      } else {
-        throw `invalid sun phase key '${matches[1]}'`;
-      }
-    } else if (matches = s.match(/^(\w+)(\+|\-)(\d+)(h|m|s)$/)) {
-      if (sunphases.hasOwnProperty(matches[1])) {
-        date = sunphases[matches[1]];
-        retval = (3600 * date.getHours()) + (60 * date.getMinutes()) + (1 * date.getSeconds());
-        switch (matches[4]) {
-          case 'h': retval += (((matches[2] == "+")?1:-1) * matches[3] * 3600)
-            break;
-          case 'm': retval += (((matches[2] == "+")?1:-1) * matches[3] * 60)
-            break;
-          case 's': retval += (((matches[2] == "+")?1:-1) * matches[3])
-            break;
-        }
-      } else {
-        throw `invalid sun phase key '${matches[1]}'`;
+function parseTimeString(s: string, sunphases: any): number {
+  var retval: number
+  var date: Date
+  var matches: RegExpMatchArray | null
+
+  if (matches = (s.trim()).match(/^(\d\d):(\d\d):(\d\d)$/)) {
+    if (((1 * Number(matches[1])) < 24) && ((1 * Number(matches[2])) < 60) && ((1 * Number(matches[3])) < 60)) {
+      retval = (3600 * Number(matches[1])) + (60 * Number(matches[2])) + (1 * Number(matches[3]));
+    } else {
+      throw "hh:mm:ss value is invalid";
+    }
+  } else if (matches = (s.trim()).match(/^(\w+)$/)) {
+    if (sunphases.hasOwnProperty(matches[1])) {
+      date = new Date(sunphases[matches[1]]);
+      retval = (3600 * date.getHours()) + (60 * date.getMinutes()) + (1 * date.getSeconds());
+    } else {
+      throw `invalid sun phase key '${matches[1]}'`;
+    }
+  } else if (matches = s.match(/^(\w+)(\+|\-)(\d+)(h|m|s)$/)) {
+    if (sunphases.hasOwnProperty(matches[1])) {
+      date = new Date(sunphases[matches[1]]);
+      retval = (3600 * date.getHours()) + (60 * date.getMinutes()) + (1 * date.getSeconds());
+      switch (matches[4]) {
+        case 'h': retval += (((matches[2] == "+")?1:-1) * Number(matches[3]) * 3600)
+          break;
+        case 'm': retval += (((matches[2] == "+")?1:-1) * Number(matches[3]) * 60)
+          break;
+        case 's': retval += (((matches[2] == "+")?1:-1) * Number(matches[3]))
+          break;
       }
     } else {
-      throw `error parsing '${s}'`;
+      throw `invalid sun phase key '${matches[1]}'`;
     }
-    return(retval);
+  } else {
+    throw `error parsing '${s}'`;
   }
-
-  return plugin
+  return(retval);
 }
+
+interface SKPlugin {
+  id: string,
+  name: string,
+  description: string,
+  schema: any,
+  uiSchema: any,
+
+  start: (options: any) => void,
+  stop: () => void
+}
+
+interface SKOptions {
+  heartbeat: number,
+  lastday: number,
+  lastposition: { latitude: number, longitude: number },
+  metadata: any[],
+  notifications: any[],
+  positionkey: string,
+  root: string,
+  times: any
+}
+
